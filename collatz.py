c87c3ebf0f3242cdcd200fc245ec01ffb4119561
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-09-24 17:35:11 +0100

    Frist draft Collatz done

---
+++ collatz.py
@@ -0,0 +1,18 @@
+# The number we will perform the Collatz operation on
+n = 20
+
+# keep looping until we reach 1.
+# assuming the collatz conjecture is true 
+while n != 1:
+    # print current n value 
+     print(n)
+     # see if n is even  
+     if n % 2 == 0:
+         # if n is even divide by two 
+        n = n / 2
+     else:
+         # if n is odd, multiply by three and add one
+        n = (3 * n) + 1
+
+# print n value       
+print(n)
